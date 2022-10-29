84ab091aedd5731cd7ce62d92995460dd4725d24
Rovshan13 <70265102+Rovshan13@users.noreply.github.com>
2022-10-29 17:44:03 +0400

    Npm added

---
+++ server.js
@@ -0,0 +1,40 @@
+// importing packages
+const express = require('express');
+const admin = require('firebase-admin');
+const bcrpyt = require('bcrypt');
+const path = require('path');
+
+// declare static path
+let staticPath = path.join(__dirname, "public");
+
+// inttializing express.js
+const app = express();
+
+//middlewares
+app.use(express.static(staticPath));
+
+//routes
+//home route
+app.get("/", (req,res) => {
+    res.sendFile(path.join(staticPath, "index.html"));
+})
+
+// signup route
+app.get('/signup',(req,res) => {
+    res.sendFile(path.join(staticPath, "signup.html"))
+})
+
+// 404 route
+app.get('/404', (req,res) => {
+    res.sendFile(path.join(staticPath, "404.html"));
+})
+
+
+app.use((req,res) => {
+    res.redirect('/404');
+})
+
+app.listen(5500, () => {
+    console.log("listening on port 5500.......");
+})
+
